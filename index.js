6ed2f2186bdcd10a8337e39cf5e446a656078855
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2017-12-01 13:34:21 +0100

    test and fix helper functions

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 var ObjectCacher = require('./lib/object-cacher');
 var wrap = require('./lib/wrap');
+var xmlChunker = require('./lib/xml-chunker');
 
 module.exports = {
     ObjectCacher: ObjectCacher,
     wrapper: wrap.wrapper,
-    unwrapper: wrap.unwrapper
+    unwrapper: wrap.unwrapper,
+    xmlChunker: xmlChunker
 };
--- test/xml-chunker-test.js
+++ test/xml-chunker-test.js
@@ -4,6 +4,8 @@ var chai = require('chai');
 var expect = chai.expect;
 var streamify = require('stream-array');
 
+var reStream = require('../index');
+
 describe('XML Chunking', function() {
     describe('xmlChunker(tags)', function() {
         it('should chop up an XML stream so each chunk becomes a complete element', function() {
@@ -20,46 +22,125 @@ describe('XML Chunking', function() {
     });
 
     describe('openPattern(tags)', function() {
-        it('should match open tags of a single kind', function() {
+        var openPattern = reStream.xmlChunker.openPattern;
+        it('should match one open tag of a single kind', function() {
+            var pattern = openPattern([ 'one' ]);
+            expect(pattern.exec('<one>')).to.deep.equal([ '<one>', 'one' ]);
+            expect(pattern.exec('<one>').index).to.equal(0);
+            expect(pattern.exec('<another><one><a-third>')).to.deep.equal([ '<one>', 'one' ]);
+            expect(pattern.exec('<another><one><a-third>').index).to.equal(9);
+            expect(pattern.test('<one >')).to.equal(true);
+            expect(pattern.test('<one attribute="value">')).to.equal(true);
+            expect(pattern.test('')).to.equal(false);
+            expect(pattern.test('<another>')).to.equal(false);
         });
 
-        it('should match open tags of multiple kinds', function() {
+        it('should match one open tag of multiple kinds', function() {
+            var pattern = openPattern([ 'one', 'two' ]);
+            expect(pattern.exec('<one>')).to.deep.equal([ '<one>', 'one' ]);
+            expect(pattern.exec('<one>').index).to.equal(0);
+            expect(pattern.exec('<two><one>')).to.deep.equal([ '<two>', 'two' ]);
+            expect(pattern.exec('<two><one>').index).to.equal(0);
+            expect(pattern.exec('<another><one><a-third>')).to.deep.equal([ '<one>', 'one' ]);
+            expect(pattern.exec('<another><one><a-third>').index).to.equal(9);
+            expect(pattern.test('<one >')).to.equal(true);
+            expect(pattern.test('<two attribute="value">')).to.equal(true);
+            expect(pattern.test('')).to.equal(false);
+            expect(pattern.test('<another>')).to.equal(false);
         });
 
         it('should match self-closing tags', function() {
+            var pattern = openPattern([ 'one', 'two' ]);
+            expect(pattern.exec('<one/>')).to.deep.equal([ '<one/>', 'one' ]);
+            expect(pattern.exec('<one/>').index).to.equal(0);
+            expect(pattern.exec('<two/><one/>')).to.deep.equal([ '<two/>', 'two' ]);
+            expect(pattern.exec('<two/><one/>').index).to.equal(0);
+            expect(pattern.exec('<another><one/><a-third>')).to.deep.equal([ '<one/>', 'one' ]);
+            expect(pattern.exec('<another><one/><a-third>').index).to.equal(9);
+            expect(pattern.test('<one />')).to.equal(true);
+            expect(pattern.test('<one attribute="value"/>')).to.equal(true);
+            expect(pattern.test('')).to.equal(false);
+            expect(pattern.test('<another/>')).to.equal(false);
         });
 
         it('should not match end tags', function() {
+            var pattern = openPattern([ 'one', 'two' ]);
+            expect(pattern.test('</one>')).to.equal(false);
+            expect(pattern.test('</two>')).to.equal(false);
+            expect(pattern.test('</one></two>')).to.equal(false);
         });
     });
 
     describe('endPattern(tags)', function() {
-        it('should match end tags of a single kind', function() {
+        var endPattern = reStream.xmlChunker.endPattern;
+        it('should match one end tag of a single kind', function() {
+            var pattern = endPattern([ 'one' ]);
+            expect(pattern.exec('</one>')).to.deep.equal([ '</one>', 'one' ]);
+            expect(pattern.exec('</one>').index).to.equal(0);
+            expect(pattern.exec('<another></one><a-third>')).to.deep.equal([ '</one>', 'one' ]);
+            expect(pattern.exec('<another></one><a-third>').index).to.equal(9);
+            expect(pattern.test('</one >')).to.equal(false);
+            expect(pattern.test('</one attribute="value">')).to.equal(false);
+            expect(pattern.test('')).to.equal(false);
+            expect(pattern.test('</another>')).to.equal(false);
         });
 
-        it('should match end tags of multiple kinds', function() {
+        it('should match one end tag of multiple kinds', function() {
+            var pattern = endPattern([ 'one', 'two' ]);
+            expect(pattern.exec('</one>')).to.deep.equal([ '</one>', 'one' ]);
+            expect(pattern.exec('</one>').index).to.equal(0);
+            expect(pattern.exec('</two></one>')).to.deep.equal([ '</two>', 'two' ]);
+            expect(pattern.exec('</two></one>').index).to.equal(0);
+            expect(pattern.exec('<another></one><a-third>')).to.deep.equal([ '</one>', 'one' ]);
+            expect(pattern.exec('<another></one><a-third>').index).to.equal(9);
+            expect(pattern.test('</one >')).to.equal(false);
+            expect(pattern.test('</two attribute="value">')).to.equal(false);
+            expect(pattern.test('')).to.equal(false);
+            expect(pattern.test('</another>')).to.equal(false);
         });
 
         it('should not match self-closing tags', function() {
+            var pattern = endPattern([ 'one', 'two' ]);
+            expect(pattern.test('<one/>')).to.equal(false);
+            expect(pattern.test('<two/><one/>')).to.deep.equal(false);
         });
 
         it('should not match open tags', function() {
+            var pattern = endPattern([ 'one', 'two' ]);
+            expect(pattern.test('<one>')).to.equal(false);
+            expect(pattern.test('<one><two>')).to.equal(false);
         });
     });
 
     describe('match(pattern, string)', function() {
+        var match = reStream.xmlChunker.match;
+        var pattern = reStream.xmlChunker.openPattern([ 'one' ]);
         it('should return a match object when there is a match', function() {
+            expect(match(pattern, '<one>')).to.deep.equal({
+                tag: '<one>',
+                name: 'one',
+                index: 0
+            });
         });
 
         it('should return null when there is no match', function() {
+            expect(match(pattern, '<another>')).to.equal(null);
         });
     });
 
     describe('selfClosing(match)', function() {
+        var selfClosing = reStream.xmlChunker.selfClosing;
         it('should return true when the tag is self-closing', function() {
+            expect(selfClosing({ tag: '<one/>' })).to.equal(true);
+            expect(selfClosing({ tag: '<one />' })).to.equal(true);
+            expect(selfClosing({ tag: '<one attribute="value"/>' })).to.equal(true);
         });
 
         it('should return false when the tag is not self-closing', function() {
+            expect(selfClosing('')).to.equal(false);
+            expect(selfClosing('<one>')).to.equal(false);
+            expect(selfClosing('</one>')).to.equal(false);
+            expect(selfClosing('<one/ >')).to.equal(false);
         });
     });
 });
