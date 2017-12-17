d0b25bca1ec0c7e12b5dd8f4053064bb36694e61
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2017-12-17 20:02:42 +0100

    add logger and regexp-chunker

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ var wrap = require('./lib/wrap');
 var xmlChunker = require('./lib/xml-chunker');
 var objectAggregator = require('./lib/object-aggregator');
 var projection = require('./lib/projection');
+var logger = require('./lib/logger');
+var regexpChunker = require('./lib/regexp-chunker');
 
 module.exports = {
     ObjectCacher: ObjectCacher,
     wrapper: wrap.wrapper,
     unwrapper: wrap.unwrapper,
     xmlChunker: xmlChunker,
+    regexpChunker: regexpChunker,
     objectAggregator: objectAggregator,
-    projection: projection
+    projection: projection,
+    logger: logger
 };
