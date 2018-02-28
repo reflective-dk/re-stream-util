895c7a9cc8f52101481e57e6a24a9b283bb52963
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2018-02-28 22:16:29 +0100

    unit test for onHeader and onFooter in unwrapper

--- test/wrap-test.js
+++ test/wrap-test.js
@@ -3,6 +3,7 @@
 var chai = require('chai');
 chai.use(require('chai-as-promised'));
 var expect = chai.expect;
+var _ = require('lodash');
 var streamify = require('stream-array');
 var streamToPromise = require('stream-to-promise');
 var through2 = require('through2');
@@ -27,6 +28,19 @@ describe('unwrapper/wrapper', function() {
                 ',13',
                 '],"1":"status: 1","2":"status: 2","3":"status: 3"}'
             ]).notify(done);
+        });
+    });
+
+    describe('unwrapper', function(done) {
+        it('should call onHeader and onFooter', function(done) {
+            var meta = {};
+            expect(streamToPromise(
+                streamify([ '{"header":"foo","objects":[1,2,3],"footer":"bar"}' ])
+                    .pipe(restream.unwrapper(null, m => _.merge(meta, m),
+                                             m => _.merge(meta, m)))
+            ).then(function() { return meta; }))
+                .to.eventually.deep.equal({ header: 'foo', footer: 'bar' })
+                .notify(done);
         });
     });
 });
